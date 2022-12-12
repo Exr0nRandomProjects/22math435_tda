0adb9f05340d786b758d1485e63163afeaf3923b
Exr0n <mail@exr0n.com>
2022-12-12 15:23:55 -0800

    ripserer.jl slow

--- main.jl
+++ main.jl
@@ -1,12 +1,13 @@
 using LinearAlgebra
-# using Plots; pythonplot() # dependency error
-using Plots; plotlyjs()
 using Ripserer
+using Plots
 using NPZ
 using Chain
 
+# println(ARGS[1])
+
 torus = @chain npzread("torus.npy") reshape(_, :, 3)
-println(torus |> size)
+println("torus size", torus |> size)
 
 # function annulus(n, r1=1, r2=2, offset=(0, 0))
 #     result = Tuple{Float64,Float64}[]
@@ -20,23 +21,24 @@ println(torus |> size)
 # end
 # 
 # data = annulus(300)
-#
-#
+# println("annulus", data |> typeof, data |> size)
 
-# println("data generated")
-# 
-# scatter(data; label="data", markersize=2, aspect_ratio=1, show=true)
-# println("plotted")
+data = torus |> eachrow |> collect 
 
-plot(torus[:, 1], torus[:, 2], torus[:, 3], seriestype=:scatter, show=true)
-println("torus plotted")
-readline()
+# data = [ i for i in size(torus)[1] ]
+# # println(data |> size)
+# println(torus |> eachrow |> collect |> typeof)
+# # println(torus |> typeof, torus |> size, torus |> eachrow |> vec |> typeof, torus |> vec |> size)
 
+println("ripping...")
+diagram_cocycles = ripserer(data; reps=true)
+println("ripped.")
 
-# 
-# scatter(data; label="data", markersize=2, aspect_ratio=1)
-# plot!(most_persistent_ho, data; label="cycle", show=true)
+exit()
 
+# plot(torus[:, 1], torus[:, 2], torus[:, 3], seriestype=:scatter, show=true)
+# println("torus plotted")
+# readline()
 
 diagram_cocycles = ripserer(data; reps=true)
 most_persistent_co = diagram_cocycles[2][end]
@@ -46,24 +48,5 @@ midpoint = (death(most_persistent_co) - birth(most_persistent_co)) / 2
 reconstructed_at_midpoint = reconstruct_cycle(filtration, most_persistent_co, midpoint)
 
 scatter(data; label="data", markersize=2, aspect_ratio=1)
+print(reconstructed_at_midpoint |> shape)
 plot!(reconstructed_at_midpoint, data; label="reconstruction", show=true)
-
-# reconstructed_at_birth = reconstruct_cycle(filtration, most_persistent_co)
-# 
-# scatter(data; label="data", markersize=2, aspect_ratio=1)
-# plot!(reconstructed_at_birth, data; label="reconstruction", show=true)
-
-
-# readline()
-
-
-
-# diagram = ripserer(data)
-# println("ripped")
-# plot(diagram, show=true)
-# 
-# 
-# most_persistent = diagram[2][end]
-# 
-# 
-# readline()
