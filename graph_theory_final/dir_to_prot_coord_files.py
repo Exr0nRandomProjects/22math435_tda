066a53a8eb9db3353e81b28336c5a20b14ebe670
Huxley Marvit <huxmarv@nuevaschool.org>
2022-12-13 14:42:53 -0800

    in the doc things

--- graph_theory_final/dir_to_prot_coord_files.py
+++ graph_theory_final/dir_to_prot_coord_files.py
@@ -2,8 +2,8 @@ from protein_to_points import protein_to_points
 import os
 import numpy as np
 
-files = os.listdir("./inp/TESTING")
+files = os.listdir("./inp/IN_THE_DOC")
 
 for f in files:
-    d = protein_to_points(f"./inp/TESTING/{f}")
-    np.save(f"./out/{f}.npy", d)
+    d = protein_to_points(f"./inp/IN_THE_DOC/{f}")
+    np.save(f"./out/IN_THE_DOC/{f}.npy", d)
