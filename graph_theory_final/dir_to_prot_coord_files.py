ec709db4e488c68b050018605aa4bdcdbd78b0cb
Huxley Marvit <huxmarv@nuevaschool.org>
2022-12-13 20:14:09 -0800

    smalllllllllllllllllllllllllllllllllllllllllllllls

--- graph_theory_final/dir_to_prot_coord_files.py
+++ graph_theory_final/dir_to_prot_coord_files.py
@@ -2,8 +2,8 @@ from protein_to_points import protein_to_points
 import os
 import numpy as np
 
-files = os.listdir("./inp/ENZYMES")
+files = os.listdir("./inp/ION_CHANNELS")
 
 for f in files:
-    d = protein_to_points(f"./inp/ENZYMES/{f}")
-    np.save(f"./out/ENZYMES_SMALL/{f}.npy", d)
+    d = protein_to_points(f"./inp/ION_CHANNELS/{f}")
+    np.save(f"./out/ION_CHANNELS_SMALL/{f}.npy", d)
