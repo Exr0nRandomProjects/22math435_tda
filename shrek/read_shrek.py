cb45338f7596de829ddc787c9df38b9f296cd777
Exr0n <mail@exr0n.com>
2022-12-13 22:09:20 -0800

    shrek

--- shrek/read_shrek.py
+++ shrek/read_shrek.py
@@ -6,9 +6,9 @@ from stl import mesh
 
 def read_stl(fname):
     shrek = mesh.Mesh.from_file(fname)
-    shrek = shrek.points.reshape(-1, 3)
+    shrek = list(shrek.points.reshape(-1, 3))
     shrek = sample(shrek, 800)
-    return shrek
+    return np.array(shrek)
 
 def read_pcd(fname):
     ret = []
@@ -17,19 +17,21 @@ def read_pcd(fname):
             if not l[0].isnumeric():
                 continue
             ret.append([float(x) for x in l.split()])
-    ret = sample(ret, int(800))
-    np.save(fname, ret)
+    ret = sample(ret, int(1200))
     return np.array(ret)
 
 if __name__ == '__main__':
-    # shrek = read_pcd('./shrek.pcd')
-    shrek = read_stl('shrek_wazowski.stl')
+    fname = 'shrek.pcd'
+    # shrek = read_stl(fname)
+    shrek = read_pcd(fname)
 
     print(shrek.shape)
+    np.save(fname, shrek)
 
     fig = plt.figure()
     prot_ax = fig.add_subplot(1, 1, 1, projection='3d')
     prot_ax.scatter(*shrek.T)
+    prot_ax.set_axis_off()
 
     plt.show()
 
